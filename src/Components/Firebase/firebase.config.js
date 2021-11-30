0e8518550fac54725310d2f0d40a24188083ff37
Ruhul Amin <ruhuliuice@gmail.com>
2021-11-30 09:32:43 +0600

    configure authentication

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCbdrP048sZmE60tm3NawomZ87J7yogmZk",
-  authDomain: "react-firebase-4279d.firebaseapp.com",
-  projectId: "react-firebase-4279d",
-  storageBucket: "react-firebase-4279d.appspot.com",
-  messagingSenderId: "726729484561",
-  appId: "1:726729484561:web:e0e7a64eeecf96c50304b0",
+  apiKey: "AIzaSyA86uzvWQk4RaPLcDNopdGMB7wy4fCOK2A",
+  authDomain: "bluesky-tour-hub.firebaseapp.com",
+  projectId: "bluesky-tour-hub",
+  storageBucket: "bluesky-tour-hub.appspot.com",
+  messagingSenderId: "706105706099",
+  appId: "1:706105706099:web:c1f603f938f7d116070c06",
 };
 export default firebaseConfig;
